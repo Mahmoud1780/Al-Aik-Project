5166968b01c1783ec5c0fad78be4e5e45553e79e
Mahmoud Ayman <109273486+Mahmoud1780@users.noreply.github.com>
2025-09-09 21:14:43 +0300

    add Specialty Services section

---
+++ src/app/Features/Pages/home/components/specialty-service-section/specialty-service-section.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-specialty-service-section',
+  imports: [],
+  templateUrl: './specialty-service-section.html',
+  styleUrl: './specialty-service-section.css'
+})
+export class SpecialtyServiceSection {
+
+}
--- src/app/Features/Pages/home/home-page/home-page.ts
+++ src/app/Features/Pages/home/home-page/home-page.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeroSection } from '../components/hero-section/hero-section';
 import { SwimmingPoolSection } from '../components/swimming-pool-section/swimming-pool-section';
 import { LandscapeSection } from '../components/landscape-section/landscape-section';
+import { SpecialtyServiceSection } from '../components/specialty-service-section/specialty-service-section';
 
 @Component({
   selector: 'app-home-page',
-  imports: [HeroSection,SwimmingPoolSection,LandscapeSection],
+  imports: [HeroSection,SwimmingPoolSection,LandscapeSection,SpecialtyServiceSection],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css'
 })
