7a261d327fd43b50f3f90997efd25beb053484a7
Mahmoud Ayman <109273486+Mahmoud1780@users.noreply.github.com>
2025-09-09 18:37:35 +0300

    add slogan video

--- src/app/Features/Pages/home/home-page/home-page.ts
+++ src/app/Features/Pages/home/home-page/home-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HeroSection } from '../components/hero-section/hero-section';
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [HeroSection],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css'
 })
